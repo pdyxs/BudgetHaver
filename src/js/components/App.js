2b2561ad214a919df6ca895bcd2bff430d3b5a45
Paul Sztajer <talk@pdyxs.wtf>
2019-02-25 17:48:18 -0500

    icon, making a docs folder for github pages

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -60,7 +60,6 @@ class App extends Component {
               </div>
               <Menu />
               <CurrentPage />
-              <Stars />
             </Fragment>
           </Route>
         </Switch>
